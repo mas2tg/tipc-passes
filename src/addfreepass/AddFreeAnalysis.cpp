4eba9e2b8a421dc4db057fc634368c3df497cf9b
Marco Azevedo <mas2tg@virginia.edu>
2021-04-29 10:37:30 -0400

    added a few improvements to the pass

--- src/addfreepass/AddFreeAnalysis.cpp
+++ src/addfreepass/AddFreeAnalysis.cpp
@@ -5,8 +5,10 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/SymbolTableListTraits.h"
+#include "llvm/Analysis/CaptureTracking.h"
 
 using namespace llvm;
+
 
 namespace {
     struct AddFreePass : public FunctionPass {
@@ -34,46 +36,48 @@ bool AddFreePass::runOnFunction(Function &F) {
     errs() << "Visiting function: " << F.getName() << "\n";
     Instruction* ret_i;
     Instruction* alloc_i;
+    bool callocPresent = false;
     
     // BACKWARDS (based on https://stackoverflow.com/questions/44274857/iterating-over-basic-blocks-in-reverse-in-llvm-function) 
     auto bb_list = &(F.getBasicBlockList());
     for (auto iter_bb = bb_list->rbegin(); iter_bb != bb_list->rend(); ++iter_bb){
 	auto inst_list = &(iter_bb->getInstList());
 	for (auto iter_i = inst_list->rbegin(); iter_i != inst_list->rend(); ++iter_i){
-		//errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
+		errs() << "Looking at " << *iter_i << " " << (iter_i)->getOpcode() << "\n";
 		
-		//if (Instruction::Call* alloc_i = dyn_cast<Instruction::Call>(*iter_i)) // DOES NOT WORK
-		//	errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
-
     		//WE LOOK FOR ret here
 		if (iter_i->getOpcode() == 1){ 
-		        //ReturnInst* ri = cast<ReturnInst>(&(*iter_i)); 
-			errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
+			//errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
 			ret_i = &(*iter_i);
-			//CallInst::CreateFree( cast<Value>(&(*iter_i)), &(*iter_i)); // TODO: first parameter is wrong
 		}
 		
-		//THIS WORKS -- sorta (we look for alloc here)
+		//THIS WORKS 
 		if (iter_i->getOpcode() == 56 ){ // 56 corresponds to Inst::Call 
 			CallBase* alloc_call = cast<CallBase>(&(*iter_i)); // NEED to get underlying value from interator first, then its reference
-			if (alloc_call->getCalledFunction()->getName().endswith("alloc")){
-				errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
-				//iter_i->getParent()->getInstList().insert(iter_i, CallInst::CreateFree( cast<Value>(&(*iter_i)) ,  &(*iter_i) )); // DOES NOT WORK
-				//CallInst::CreateFree( cast<Value>(&(*iter_i)), &(*iter_i)); // DOES NOT WORK
-				//cast<CallInst>(CallInst::CreateFree( cast<Value>(&(*iter_i)), iter_i->getParent())    )->setTailCall(false); // DOES NOT WORK
+			//errs() << "    about to call getName("<< alloc_call->getCalledFunction() <<" )\n";
+
+			if (alloc_call->getCalledFunction() != NULL && alloc_call->getCalledFunction()->getName().endswith("alloc")){
+				// errs() << *iter_i << " " << (iter_i)->getOpcode() << "\n";
+				//errs() << "about to "
 				alloc_i = &(*iter_i);
+				if (PointerMayBeCaptured(alloc_i, false, false)){ //2nd: return; 3rd: stored
+					errs() << "    This pointer is captured!\n";
+					callocPresent = true; //False positive?
+				}
 			}
-			//errs() << alloc_call->getCalledFunction()->getName() << "\n";
 		}
 		
 
 	}
-	errs() << "\n";
     }
 
-    // add free 
-    CallInst::CreateFree( cast<Value>(alloc_i), ret_i); 
-    
+    if (callocPresent){
+   	 errs() << "Adding free()...\n";
+   	 // add free
+	 // REMEMBER: this pass does not work interprocedurally! alloc_i will never be called 
+   	 CallInst::CreateFree( cast<Value>(alloc_i), ret_i); // insert before ret_i
+    }
+    errs() << "\n";
     
     
     
@@ -91,6 +95,5 @@ bool AddFreePass::runOnFunction(Function &F) {
     return false; 
 }
 
-// Register the pass with llvm, so that we can call it with fvpass
 char AddFreePass::ID = 0;
 static RegisterPass<AddFreePass> X("afpass", "--pending explanation--");
