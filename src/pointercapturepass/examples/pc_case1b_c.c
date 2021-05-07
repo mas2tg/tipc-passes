5feb38dea65caabadccf5b6b963c0e7fff71e3ef
Marco Azevedo <mas2tg@virginia.edu>
2021-05-07 08:17:13 -0400

    added evaluation examples

---
+++ src/pointercapturepass/examples/pc_case1b_c.c
@@ -0,0 +1,14 @@
+long foo(long * arg){ 
+	long x = *arg; // arg does not outlive foo()
+	long *z = &x; // z does not outlive foo()
+	return x;
+}
+
+int main(){
+	long x;
+	long *y;
+	x = 0;
+	y = &x;
+	x = foo(y);
+	return 0;
+}
